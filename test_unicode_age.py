1d89e7e4b0127e3b3ef483f41f1f029c473abd36
James Gerity <snoopjedi@gmail.com>
2022-10-09 17:36:52 -0400

    Add some simple tests

---
+++ test_unicode_age.py
@@ -0,0 +1,17 @@
+import unicode_age
+
+import pytest
+
+
+def test_version():
+    assert unicode_age.version(ord("\N{SNAKE}")) == (6, 0)
+
+
+def test_version_endofspan():
+    # Regression test: at one point, the ends of version spans given in DerivedAge.txt weren't being handled correctly
+    assert unicode_age.version(ord("\N{DEAF PERSON}")) == (12, 0)
+
+
+def test_version_badtype():
+    with pytest.raises(TypeError):
+        unicode_age.version("\N{SNAKE}")
