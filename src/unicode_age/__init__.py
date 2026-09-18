75ae61443ac8046b571b0856ed848c33f74edd23
Jeff Epler <jepler@unpythonic.net>
2026-09-18 16:58:38 -0500

    Use bisect search

--- src/unicode_age/__init__.py
+++ src/unicode_age/__init__.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import bisect
+import functools
 from typing import Tuple
 
 from .unicode_age_db import iter_spans, UCD_VERSION
@@ -8,11 +10,19 @@ UCD_VERSION_STR = ".".join(str(it) for it in UCD_VERSION)
 
 UCDVersion = Tuple[int, int]
 
+@functools.cache
+def _spans():
+    return list(iter_spans())
+
+
 
 def version(codept: int) -> UCDVersion | None:
-    for (start, stop, major, minor) in iter_spans():
+    spans = _spans()
+    idx = bisect.bisect_right(spans, (codept+1, codept))
+    if idx != 0:
+        start, stop, major, minor = spans[idx-1]
         if start <= codept <= stop:
             return (major, minor)
 
-    # linear scan failed
+    # search failed
     raise ValueError(f"Codepoint U+{codept:x} was not allocated as of UCD {UCD_VERSION_STR}")
